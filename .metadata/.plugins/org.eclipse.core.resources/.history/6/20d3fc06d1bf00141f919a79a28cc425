76a9a4520b7ea41f68c359407b1da292c1de113b
jenny350026 <jenny350026@hotmail.com>
2015-02-28 23:18:09 -0600

    Adding Initial Classes
    Attraction.java
    YelpQuery.java
    WeatherQuery.java
    AttractionDatabase.java

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/6/20d3fc06d1bf00141f919a79a28cc425
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+
+
+public class YelpQuery {
+	//populate your own private
+	
+	public YelpQuery(String start_location, int distance){
+		
+	}
+	
+	public ArrayList<Attraction> getAttractions(){
+		return new ArrayList<Attraction>();
+	}
+}
---
+++ EpiphanyTrip/src/AttractionDatabase.java
@@ -0,0 +1,32 @@
+import java.util.ArrayList;
+import java.util.Date;
+
+
+public class AttractionDatabase {
+	private ArrayList<Attraction> attractions;
+	
+	public AttractionDatabase(String start_location, Date date, int distance){
+		
+	}
+	
+	public ArrayList<Attraction> filterByDistance(int d){
+		
+		return new ArrayList<Attraction>();
+	}
+	
+	public ArrayList<Attraction> filterByWeather(String w){
+
+		return new ArrayList<Attraction>();
+	}
+	
+	public ArrayList<Attraction> filterByRating(double r) {
+
+		return new ArrayList<Attraction>();
+	}
+	
+	public ArrayList<Attraction> filterByCityName(String n){
+		
+		return new ArrayList<Attraction>();
+	}
+
+}
